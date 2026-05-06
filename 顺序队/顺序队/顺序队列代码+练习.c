cdea631e7d139562c52966851729357a28f13a5c
Jocelyn <16488534+q2357053630@user.noreply.gitee.com>
2026-05-06 09:19:40 +0000

    update 顺序队/顺序队/顺序队列代码+练习.c.
    Signed-off-by: Jocelyn <16488534+q2357053630@user.noreply.gitee.com>

--- 顺序队/顺序队/顺序队列代码+练习.c
+++ 顺序队/顺序队/顺序队列代码+练习.c
@@ -1,42 +1,42 @@
-//#include<stdio.h>
-//#include<stdlib.h>
-//#define maxx 10
-//typedef struct Queue {
-//	int* data;
-//	int f, r;
-//}Queue;
-//Queue InitQueue() {
-//	Queue q;
-//	q.data = (int*)malloc(sizeof(int) * maxx);
-//	q.f = q.r = 0;
-//	return q;
-//}
-//void EnQueue(Queue* q, int x) {
-//	if (q->r == maxx) {
-//		printf("队列已满，无法入队\n");
-//		return;
-//	}
-//	else {
-//		q->data[q->r] = x;
-//		q->r++;
-//	}
-//}
-//void DeQueue(Queue* q) {
-//	if (q->f == q->r) {
-//		printf("队列已空，无法出队\n");
-//		return;
-//	}
-//	else {
-//		printf("%d出队\n", q->data[q->f]);
-//		q->f++;
-//	}
-//}
-//int main() {
-//	Queue q = InitQueue();
-//	EnQueue(&q, 1);
-//	EnQueue(&q, 2);
-//	EnQueue(&q, 3);
-//	DeQueue(&q);
-//	DeQueue(&q);
-//	return 0;
-//}
+#include<stdio.h>
+#include<stdlib.h>
+#define maxx 10
+typedef struct Queue {
+	int* data;
+	int f, r;
+}Queue;
+Queue InitQueue() {
+	Queue q;
+	q.data = (int*)malloc(sizeof(int) * maxx);
+	q.f = q.r = 0;
+	return q;
+}
+void EnQueue(Queue* q, int x) {
+	if (q->r == maxx) {
+		printf("闃熷垪宸叉弧锛屾棤娉曞叆闃焅n");
+		return;
+	}
+	else {
+		q->data[q->r] = x;
+		q->r++;
+	}
+}
+void DeQueue(Queue* q) {
+	if (q->f == q->r) {
+		printf("闃熷垪宸茬┖锛屾棤娉曞嚭闃焅n");
+		return;
+	}
+	else {
+		printf("%d鍑洪槦\n", q->data[q->f]);
+		q->f++;
+	}
+}
+int main() {
+	Queue q = InitQueue();
+	EnQueue(&q, 1);
+	EnQueue(&q, 2);
+	EnQueue(&q, 3);
+	DeQueue(&q);
+	DeQueue(&q);
+	return 0;
+}
